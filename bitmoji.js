53686587081e9d7fa9b9eca4f52ac8044c22cf23
matthewgil <71112314+matthewgil@users.noreply.github.com>
2020-10-06 01:08:56 -0400

    Add files via upload

---
+++ bitmoji.js
@@ -0,0 +1,32 @@
+noStroke();
+fill(122, 1, 1);
+ellipse(219, 167, 106, 119);//hoodie
+fill(255, 224, 189);
+ellipse(221,169,91,100);//head
+fill(122, 1, 1);
+rect(179, 219, 85, 107);//body
+
+stroke(0, 0, 0);//black glasses
+fill(255, 224, 189);//skin color
+rect(187,150,31,21);//glasses-left
+rect(226,150,31,21);//glasses-right
+line(219,159,228,160);//nose bridge
+line(267,154,256,160);//glasses left
+line(188,164,178,154);//glasses right
+fill(54, 19, 0);
+ellipse(202,159,23,14);//eyes-lefty
+fill(54, 19, 0);
+ellipse(240,159,23,14);//eyes-righty
+line(253,193,221,190);//mouth
+
+
+line(193,239,185,284);//m
+line(195,238,204,280);//m
+line(226,235,202,280);//m
+line(225,240,225,277);//m
+
+line(235,239,259,234);//g
+line(234,278,236,239);//g
+line(234,278,262,275);//g
+line(258,247,259,275);//g
+line(258,249,243,251);//g
---
+++ dancing_bitmoji.js
@@ -0,0 +1,36 @@
+ var x= random(30,230);
+var y=random(30,230);
+
+
+noStroke();
+fill(122, 1, 1);
+ellipse(x+119, y+67, 106, 119);//hoodie
+fill(255, 224, 189);
+ellipse(x+121,y+69,91,100);//head
+fill(122, 1, 1);
+rect(x+79, y+119, 85, 107);//body
+
+stroke(0, 0, 0);//black glasses
+fill(255, 224, 189);//skin color
+rect(x+87,y+50,31,21);//glasses-left
+rect(x+126,y+50,31,21);//glasses-right
+line(x+119,y+59,x+128,y+60);//nose bridge
+line(x+167,y+54,x+156,y+60);//glasses wire left
+line(x+88,y+64,x+78,y+54);//glasses wire right
+fill(54, 19, 0);
+ellipse(x+102,y+59,23,14);//eyes-lefty
+fill(54, 19, 0);
+ellipse(x+140,y+59,23,14);//eyes-righty
+line(x+153,y+93,x+121,y+90);//mouth
+
+
+line(x+93,y+139,x+85,y+184);//m
+line(x+95,y+138,x+104,y+180);//m
+line(x+119,y+141,x+104,y+180);//m
+line(x+119,y+138,x+124,y+182);//m
+
+line(x+135,y+139,x+159,y+134);//g
+line(x+134,y+178,x+136,y+139);//g
+line(x+134,y+178,x+162,y+175);//g
+line(x+158,y+147,x+159,y+175);//g
+line(x+158,y+149,x+143,y+151);//g
